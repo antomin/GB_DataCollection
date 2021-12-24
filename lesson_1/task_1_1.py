686e93e289737744cb6f5f0d8c1293c05896528e
Anton Mindlin <anton.mindlin@gmail.com>
2021-12-24 17:15:00 +0200

    Lesson_1 homework complited

---
+++ lesson_1/task_1_1.py
@@ -0,0 +1,19 @@
+"""
+1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
+сохранить JSON-вывод в файле *.json.
+"""
+
+import requests
+
+user = 'antomin'  # Enter username here
+url = f'https://api.github.com/users/{user}/repos'
+responce = requests.get(url)
+
+if responce.ok:
+    with open('data.json', 'w') as new_json:
+        new_json.write(responce.text)  # Не уверен, что можно в json хранить список словарей. Прокоментируйте, пожалуйста.
+
+    for el in responce.json():
+        print(el['name'])
+else:
+    print(f'User "{user}" not found.')
